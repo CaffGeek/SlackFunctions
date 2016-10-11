07930c4f9db248391a198eaf7b6333bf408cf168
Chad Hurd <gibble@gmail.com>
2016-10-10 22:26:41 -0500

    restructure folders

---
+++ Hal9000/lib/hal9000.js
@@ -0,0 +1,48 @@
+var util = require('util');
+var path = require('path');
+var fs = require('fs');
+var Bot = require('slackbots');
+
+var Hal9000 = function Constructor(settings) {
+    this.settings = settings;
+    this.settings.name = this.settings.name || 'hal9000';
+};
+
+util.inherits(Hal9000, Bot);
+
+Hal9000.prototype.run = function () {
+    Hal9000.super_.call(this, this.settings);
+
+    this.on('start', this._onStart);
+    this.on('message', this._onMessage);
+};
+
+Hal9000.prototype._onStart = function () {
+    this._loadBotUser();
+};
+
+Hal9000.prototype._onMessage = function (message) {    
+    var self = this;
+    
+	var channel = self._getChannelById(message.channel);
+	self.postMessageToChannel(channel.name, "Hi Dave!", {as_user: true});
+};
+
+Hal9000.prototype._loadBotUser = function () {
+    var self = this;
+    this.user = this.users.filter(function (user) {
+        return user.name === self.name;
+    })[0];
+};
+
+module.exports = Hal9000;
+
+var token = process.env.BOT_API_KEY;
+var name = process.env.BOT_NAME;
+
+var norrisbot = new Hal9000({
+    token: token,
+    name: name
+});
+
+norrisbot.run();
