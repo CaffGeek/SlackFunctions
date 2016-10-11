edcf42a82a8162393eb2ede3a9eac65f56976f27
Chad Hurd <gibble@gmail.com>
2016-10-10 19:26:06 -0500

    asdf

--- Hal9000/run.js
+++ Hal9000/run.js
@@ -1,6 +1,8 @@
 module.exports = function(context, req) {
     context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
     context.log('query.text=%s', req.query.text);
+    context.log('query.body=%s', req.body);
+    context.log('query.body.text=%s', req.body.text);
     
 	var input = req.query.text;
 	var command = input ? input.split(' ')[0] : '';
