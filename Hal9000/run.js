5032cb5f8dc55ffdc5e52aa101b52259c6060470
Chad Hurd <gibble+github@gmail.com>
2016-10-11 11:39:09 -0500

    Update run.js

--- Hal9000/run.js
+++ Hal9000/run.js
@@ -2,10 +2,17 @@
 
 'use strict';
 
+context.log('Hal9000 coming online...');
+
 var Hal9000 = require('./lib/hal9000');
 
 var token = process.env.BOT_API_KEY;
 var name = process.env.BOT_NAME;
+
+context.log('Token=%s', token);
+context.log('Name=%s', name);
+
+context.log('Hal9000 coming online....');
 
 var hal9000 = new Hal9000({
     token: token,
