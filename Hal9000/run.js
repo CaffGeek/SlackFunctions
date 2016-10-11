f12a0ae7d86544c43796cae9d238145f988434a5
Chad Hurd <gibble@gmail.com>
2016-10-10 21:35:36 -0500

    start writing hal

--- Hal9000/run.js
+++ Hal9000/run.js
@@ -1,42 +1,40 @@
+'use strict';
+
+var util = require('util');
+var path = require('path');
+var fs = require('fs');
 var Bot = require('slackbots');
 
-module.exports = function(context, req) {
-    context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
-    
-	if (req.body && req.body.challenge){
-		context.res = {
-			challenge: req.body.challenge
-		};
-    	context.done();
-	}
-    
-	var input = req.query.text;
-	var command = input ? input.split(' ')[0] : '';
-		
-	switch (command) {
-		case 'time': 
-			context.res = {
-				body: { 
-					response_type: "in_channel",
-					text:  "Hello, it's " + (new Date())
-				}
-			};
-			break;
-		case 'wordcloud':
-			context.res = {
-				body: { 
-					response_type: "in_channel",
-					text:  "wordcloud"
-				}
-			};
-			break;
-		default:			
-			context.res = {
-				status: 400,
-				body:  "I'm sorry, I can't let you do that."
-			};
-			break;
-	}
-	
-    context.done();
+var Hal9000 = function Constructor(settings) {
+    this.settings = settings;
+    this.settings.name = this.settings.name || 'hal9000';
 };
+
+util.inherits(Hal9000, Bot);
+
+Hal9000.prototype.run = function () {
+    Hal9000.super_.call(this, this.settings);
+
+    this.on('start', this._onStart);
+    this.on('message', this._onMessage);
+};
+
+Hal9000.prototype._onStart = function () {
+    this._loadBotUser();
+};
+
+Hal9000.prototype._onMessage = function (message) {    
+    var self = this;
+    
+	var channel = self._getChannelById(message.channel);
+	self.postMessageToChannel(channel.name, "Hi Dave!", {as_user: true});
+};
+
+Hal9000.prototype._loadBotUser = function () {
+    var self = this;
+    this.user = this.users.filter(function (user) {
+        return user.name === self.name;
+    })[0];
+};
+
+module.exports = Hal9000;
