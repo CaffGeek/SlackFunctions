8fe34831f040dc824eea2fc8823c935e9d0c7783
caffgeek <gibble@gmail.com>
2016-10-12 21:08:55 -0500

    adding oauth structure

--- Hal9000/run.js
+++ Hal9000/run.js
@@ -1,18 +1,19 @@
 module.exports = function(context, req) {
-    context.log('Hal9000 coming online.');
-
     if (req.body && req.body.challenge) {
+        context.log('Challenged.');
+
         context.log('query.body.challenge=%s', req.body.challenge);
         context.res = { challenge: req.body.challenge };
      	context.done();
  	}
-    
-    if (req.query && req.query.code)
+
+    if (req.query && req.query.do == "auth" && req.query.code) {
+        context.log('Authenticating OAuth.');
         context.log('Code=%s', req.query.code);
+
+    }
     
-    if (req.query && req.query.code2)
-        context.log('Code2=%s', req.query.code2);
-    
+    context.log('Hal9000 coming online.');
     var Hal9000 = require('./lib/hal9000');
 
     var token = process.env.BOT_API_KEY;
@@ -20,7 +21,6 @@ module.exports = function(context, req) {
 
     context.log('Token=%s', token);
     context.log('Name=%s', name);
-
 
     var hal9000 = new Hal9000({
         token: token,
