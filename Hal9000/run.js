edd3aba93ad2197106c8ae52f976517893364b56
caffgeek <gibble@gmail.com>
2016-10-12 21:29:36 -0500

    whoops, wrong name

--- Hal9000/run.js
+++ Hal9000/run.js
@@ -1,12 +1,12 @@
 'use strict';
 
 var config = require('./config.js');
-var GerardBot = require('lib/gerardbot.js');
+var Hal9000 = require('lib/hal9000.js');
 
-var gerardbot = new GerardBot(
+var hal9000 = new Hal9000(
 {
 	token: config.bot_api_key,
 	name: config.botname
 });
 
-gerardbot.run();
+hal9000.run();
